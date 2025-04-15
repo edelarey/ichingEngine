bb844c9cd86415e19c29b1619bfe578d8f25e1de
Edward de la Rey <edward@itbsoftware.co.za>
2025-04-15 14:37:05 +0200

    Add images for astrological animals
    On Summary Only at this point

--- src/views/astrology.vue
+++ src/views/astrology.vue
@@ -50,41 +50,65 @@
       </ul>
 
       <div class="tab-content" id="astrologyTabContent">
-        <div class="tab-pane fade show active" id="summary" role="tabpanel" aria-labelledby="summary-tab">
-        <div class="row justify-content-center mt-4">
-          <div class="col-12 col-md-10 col-lg-8">
-            <div class="card text-center">
-              <h3 class="card-header py-3">I Ching Astrology Summary</h3>
-              <div class="card-body">
-                <div v-if="state.cycle">
-                  <h5 class="card-title mb-3">Personal Details</h5>
-                  <p><strong>Name:</strong> {{ state.name }}</p>
-                  <p><strong>Birth Date:</strong> {{ dateTimeFormatSimple(state.birthDate) }}</p>
-                  <p><strong>Gender:</strong> {{ state.gender }}</p>
-                  <p><strong>Hemisphere:</strong> {{ state.hemisphere }}</p>
-                  <h5 class="card-title mt-4 mb-3">Core Hexagrams</h5>
-                  <p><strong>Pre-Heaven Hexagram:</strong> <router-link :to="`/hexagram_detail?hexagram=${state.preHeavenHexagram?.binary}`">{{ state.preHeavenHexagram?.name }}</router-link> ({{ state.preHeavenHexagram?.symbol }}) ({{ state.preHeavenHexagram?.hexagram }})</p>
-                  <p><strong>Later-Heaven Hexagram:</strong> <router-link :to="`/hexagram_detail?hexagram=${state.laterHeavenHexagram?.binary}`">{{ state.laterHeavenHexagram?.name }}</router-link> ({{ state.laterHeavenHexagram?.symbol }}) ({{ state.laterHeavenHexagram?.hexagram }})</p>
-                  <p><strong>Time of Birth Hexagram:</strong> <router-link :to="`/hexagram_detail?hexagram=${state.timeOfBirthHexagram?.hexagram.binary}`">{{ state.timeOfBirthHexagram?.hexagram.name }}</router-link> ({{ state.timeOfBirthHexagram?.hexagram.symbol }}) ({{ state.timeOfBirthHexagram?.hexagram.hexagram }})</p>
-                  <h5 class="card-title mt-4 mb-3">Trigrams</h5>
-                  <p><strong>Heavenly Trigram:</strong> <router-link :to="`/trigram_detail?trigram=${state.heavenlyTrigram?.trigram.binary}`">{{ state.heavenlyTrigram?.trigram.name }}</router-link> ({{ state.heavenlyTrigram?.trigram.trigram }})</p>
-                  <p><strong>Earthly Trigram:</strong> <router-link :to="`/trigram_detail?trigram=${state.earthlyTrigram?.trigram.binary}`">{{ state.earthlyTrigram?.trigram.name }}</router-link> ({{ state.earthlyTrigram?.trigram.trigram }})</p>
-                  <h5 class="card-title mt-4 mb-3">Current Life Stage</h5>
-                  <p v-if="state.selectedPreHeavenBirthSubCycle"><strong>Early Life:</strong> <router-link :to="`/hexagram_detail?hexagram=${state.selectedPreHeavenBirthSubCycle.hexagram.binary}`">{{ state.selectedPreHeavenBirthSubCycle.hexagram.name }}</router-link> ({{ state.selectedPreHeavenBirthSubCycle.hexagram.hexagram }}) (Age {{ state.selectedPreHeavenBirthSubCycle.age }})</p>
-                  <p v-if="state.selectedLaterHeavenBirthSubCycle"><strong>Later Life:</strong> <router-link :to="`/hexagram_detail?hexagram=${state.laterHeavenBirthSubCycleHexagram?.binary}`">{{ state.laterHeavenBirthSubCycleHexagram?.name }}</router-link> ({{ state.laterHeavenBirthSubCycleHexagram?.hexagram }}) (Age {{ state.selectedLaterHeavenBirthSubCycle.age }})</p>
-                  <h5 class="card-title mt-4 mb-3">Cycles</h5>
-                  <p><strong>Yearly Cycle ({{ formatBirthYear }}):</strong> {{ state.sexagenaryCycle?.celestialStem.name }} (<router-link :to="`/trigram_detail?trigram=${state.sexagenaryCycle?.celestialStem.trigram.binary}`">{{ state.sexagenaryCycle?.celestialStem.trigram.trigram }}</router-link>) - {{ state.sexagenaryCycle?.horaryBranch.name }} ({{ state.sexagenaryCycle?.horaryBranch.animal }})</p>
-                  <p><strong>Monthly Cycle ({{ formatBirthMonth }}):</strong> {{ state.birthStemsandBranches?.celestialStem.name }} (<router-link :to="`/trigram_detail?trigram=${state.birthStemsandBranches?.celestialStem.trigram.binary}`">{{ state.birthStemsandBranches?.celestialStem.trigram.trigram }}</router-link>) - {{ state.birthStemsandBranches?.horaryBranch.name }} ({{ state.birthStemsandBranches?.horaryBranch.animal }})</p>
-                  <p><strong>Daily Cycle ({{ formatBirthDay }}):</strong> {{ state.dailyStemsandBranches?.celestialStem.name }} (<router-link :to="`/trigram_detail?trigram=${state.dailyStemsandBranches?.celestialStem.trigram.binary}`">{{ state.dailyStemsandBranches?.celestialStem.trigram.trigram }}</router-link>) - {{ state.dailyStemsandBranches?.horaryBranch.name }} ({{ state.dailyStemsandBranches?.horaryBranch.animal }})</p>
-                </div>
-                <div v-else>
-                  <p>Please enter birth details and consult to view your summary.</p>
-                </div>
-              </div>
+  <div class="tab-pane fade show active" id="summary" role="tabpanel" aria-labelledby="summary-tab">
+    <div class="row justify-content-center mt-4">
+      <div class="col-12 col-md-10 col-lg-8">
+        <div class="card text-center">
+          <h3 class="card-header py-3">I Ching Astrology Summary</h3>
+          <div class="card-body">
+            <div v-if="state.cycle">
+              <h5 class="card-title mb-3">Personal Details</h5>
+              <p><strong>Name:</strong> {{ state.name }}</p>
+              <p><strong>Birth Date:</strong> {{ dateTimeFormatSimple(state.birthDate) }}</p>
+              <p><strong>Gender:</strong> {{ state.gender }}</p>
+              <p><strong>Hemisphere:</strong> {{ state.hemisphere }}</p>
+              <h5 class="card-title mt-4 mb-3">Core Hexagrams</h5>
+              <p><strong>Pre-Heaven Hexagram:</strong> <router-link :to="`/hexagram_detail?hexagram=${state.preHeavenHexagram?.binary}`">{{ state.preHeavenHexagram?.name }}</router-link> ({{ state.preHeavenHexagram?.symbol }}) ({{ state.preHeavenHexagram?.hexagram }})</p>
+              <p><strong>Later-Heaven Hexagram:</strong> <router-link :to="`/hexagram_detail?hexagram=${state.laterHeavenHexagram?.binary}`">{{ state.laterHeavenHexagram?.name }}</router-link> ({{ state.laterHeavenHexagram?.symbol }}) ({{ state.laterHeavenHexagram?.hexagram }})</p>
+              <p><strong>Time of Birth Hexagram:</strong> <router-link :to="`/hexagram_detail?hexagram=${state.timeOfBirthHexagram?.hexagram.binary}`">{{ state.timeOfBirthHexagram?.hexagram.name }}</router-link> ({{ state.timeOfBirthHexagram?.hexagram.symbol }}) ({{ state.timeOfBirthHexagram?.hexagram.hexagram }})</p>
+              <h5 class="card-title mt-4 mb-3">Trigrams</h5>
+              <p><strong>Heavenly Trigram:</strong> <router-link :to="`/trigram_detail?trigram=${state.heavenlyTrigram?.trigram.binary}`">{{ state.heavenlyTrigram?.trigram.name }}</router-link> ({{ state.heavenlyTrigram?.trigram.trigram }})</p>
+              <p><strong>Earthly Trigram:</strong> <router-link :to="`/trigram_detail?trigram=${state.earthlyTrigram?.trigram.binary}`">{{ state.earthlyTrigram?.trigram.name }}</router-link> ({{ state.earthlyTrigram?.trigram.trigram }})</p>
+              <h5 class="card-title mt-4 mb-3">Current Life Stage</h5>
+              <p v-if="state.selectedPreHeavenBirthSubCycle"><strong>Early Life:</strong> <router-link :to="`/hexagram_detail?hexagram=${state.selectedPreHeavenBirthSubCycle.hexagram.binary}`">{{ state.selectedPreHeavenBirthSubCycle.hexagram.name }}</router-link> ({{ state.selectedPreHeavenBirthSubCycle.hexagram.hexagram }}) (Age {{ state.selectedPreHeavenBirthSubCycle.age }})</p>
+              <p v-if="state.selectedLaterHeavenBirthSubCycle"><strong>Later Life:</strong> <router-link :to="`/hexagram_detail?hexagram=${state.laterHeavenBirthSubCycleHexagram?.binary}`">{{ state.laterHeavenBirthSubCycleHexagram?.name }}</router-link> ({{ state.laterHeavenBirthSubCycleHexagram?.hexagram }}) (Age {{ state.selectedLaterHeavenBirthSubCycle.age }})</p>
+              <h5 class="card-title mt-4 mb-3">Cycles</h5>
+              <p>
+                <strong>Yearly Cycle ({{ formatBirthYear }}):</strong> 
+                {{ state.sexagenaryCycle?.celestialStem.name }} 
+                (<router-link :to="`/trigram_detail?trigram=${state.sexagenaryCycle?.celestialStem.trigram.binary}`">{{ state.sexagenaryCycle?.celestialStem.trigram.trigram }}</router-link>) 
+                - {{ state.sexagenaryCycle?.horaryBranch.name }} 
+                ({{ state.sexagenaryCycle?.horaryBranch.animal }}) 
+                <img :src="`/images/${state.sexagenaryCycle?.horaryBranch.animal.toLowerCase()}.jpg`" alt="Yearly Cycle Animal" style="width: 100px; height: 100px; margin-left: 10px; vertical-align: middle;" />
+              </p>
+              <p>
+                <strong>Monthly Cycle ({{ formatBirthMonth }}):</strong> 
+                {{ state.birthStemsandBranches?.celestialStem.name }} 
+                (<router-link :to="`/trigram_detail?trigram=${state.birthStemsandBranches?.celestialStem.trigram.binary}`">{{ state.birthStemsandBranches?.celestialStem.trigram.trigram }}</router-link>) 
+                - {{ state.birthStemsandBranches?.horaryBranch.name }} 
+                ({{ state.birthStemsandBranches?.horaryBranch.animal }}) 
+                <img :src="`/images/${state.birthStemsandBranches?.horaryBranch.animal.toLowerCase()}.jpg`" alt="Monthly Cycle Animal" style="width: 100px; height: 100px; margin-left: 10px; vertical-align: middle;"/>
+              </p>
+              <p>
+                <strong>Daily Cycle ({{ formatBirthDay }}):</strong> 
+                {{ state.dailyStemsandBranches?.celestialStem.name }} 
+                (<router-link :to="`/trigram_detail?trigram=${state.dailyStemsandBranches?.celestialStem.trigram.binary}`">{{ state.dailyStemsandBranches?.celestialStem.trigram.trigram }}</router-link>) 
+                - {{ state.dailyStemsandBranches?.horaryBranch.name }} 
+                ({{ state.dailyStemsandBranches?.horaryBranch.animal }}) 
+                <img :src="`/images/${state.dailyStemsandBranches?.horaryBranch.animal.toLowerCase()}.jpg`" alt="Daily Cycle Animal" style="width: 100px; height: 100px; margin-left: 10px; vertical-align: middle;" />
+              </p>
+            </div>
+            <div v-else>
+              <p>Please enter birth details and consult to view your summary.</p>
             </div>
           </div>
         </div>
       </div>
+    </div>
+  </div>
+
+     
+
          <!-- Birthday History Tab -->
         <div class="tab-pane fade" id="birthday-history" role="tabpanel" aria-labelledby="birthday-history-tab">
           <div class="row justify-content-center mt-4">
@@ -1158,3 +1182,5 @@
     },
   };
   </script>
+
+
